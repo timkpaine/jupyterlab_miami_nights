a8c090c12c2bf08689614ec8db7d2ecaa9c1e657
Tim Paine <t.paine154@gmail.com>
2020-06-22 12:11:17 -0400

    migrate from other package

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/**
+ * A flat, 80's neon inspired theme for JupyterLab.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: '@timkpaine/jupyterlab_miami_nights:plugin',
+  // id: '@yeebc/jupyterlab_neon_theme:plugin',
+  requires: [IThemeManager],
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+    const style = '@yeebc/jupyterlab_neon_theme/index.css';
+
+    // manager.register({
+    //   name: 'JupyterLab Neon Night',
+    //   isLight: false,
+    //   themeScrollbars: true,
+    //   load: () => manager.loadCSS(style),
+    //   unload: () => Promise.resolve(undefined)
+    // });
+
+
+    manager.register({
+      name: 'JupyterLab Miami Nights',
+      isLight: false,
+      themeScrollbars: true,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  },
+  autoStart: true
+};
+
+export default plugin;
