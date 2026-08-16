5605117ce1235cf439f2d8350ae753267d3a5049
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-08-16 10:48:07 -0400

    resolve conflicts, fix lint

--- js/tests/activate.test.js
+++ js/tests/activate.test.js
@@ -4,11 +4,7 @@ import plugin from "../src/index";
 
 describe("Checks activate", () => {
   test("Check activate", () => {
-<<<<<<< before updating
     const {activate} = plugin;
-    expect(activate);
-=======
-    expect(_activate).toBeDefined();
->>>>>>> after updating
+    expect(activate).toBeInstanceOf(Function);
   });
 });
